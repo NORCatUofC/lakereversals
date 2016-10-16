e739eb2f26cebc67771a9cf9ed28106836055d8f
sbeslow <bezzy21@yahoo.com>
2016-10-15 20:58:31 -0500

    Added counter of total sewage

--- js/swimmingInSewage.js
+++ js/swimmingInSewage.js
@@ -1,10 +1,25 @@
 function buildAndDisplayData(data) {
     var total = 0;
-    for (var i=0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         total += parseFloat(data[i].total);
     }
     // Convert total to gallons (from millions of gallons) and an integer
     total = parseInt(total * 1000000)
-    alert(total);
+    setupCounter(total);
+
+}
+
+function setupCounter(total) {
     
+    var total_start = (total-100000).toLocaleString();
+
+    $('#theCounter').addClass('counter-analog').counter({
+        initial: total_start,
+        direction: 'up',
+        interval: '1',
+        format: total.toLocaleString(),
+        stop: total.toLocaleString()
+    });
+
+
 }
