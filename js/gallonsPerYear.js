140dadea9afeb5f41000541e7c077044d7619b40
sbeslow <bezzy21@yahoo.com>
2016-10-15 22:25:49 -0500

    High charts code working for vertical and horizontal bar charts.  Needs cleanup

---
+++ js/gallonsPerYear.js
@@ -0,0 +1,84 @@
+// High charts
+
+
+
+function drawGallonsPerYearChart(years, series) {
+    $('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Stacked bar chart'
+        },
+        xAxis: {
+            categories: years
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total fruit consumption'
+            }
+        },
+        legend: {
+            reversed: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: series
+    });
+
+    /*
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Gallons Per Year'
+        },
+        xAxis: {
+            categories: years
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Sewage dumped (millions of gallons)'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            headerFormat: '<b>{point.x}</b><br/>',
+            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                }
+            }
+        },
+        series: series,
+    });
+    */
+}
--- js/swimmingInSewage.js
+++ js/swimmingInSewage.js
@@ -1,17 +1,20 @@
 function buildAndDisplayData(data) {
     var total = 0;
+
     for (var i = 0; i < data.length; i++) {
         total += parseFloat(data[i].total);
     }
     // Convert total to gallons (from millions of gallons) and an integer
     total = parseInt(total * 1000000)
-    setupCounter(total);
+    showCounter(total);
+    var a = gallonsPerStationSeries(data);;
+    drawGallonsPerYearChart(a.years, a.series);
 
 }
 
-function setupCounter(total) {
-    
-    var total_start = (total-100000).toLocaleString();
+function showCounter(total) {
+
+    var total_start = (total - 100000).toLocaleString();
 
     $('#theCounter').addClass('counter-analog').counter({
         initial: total_start,
@@ -20,6 +23,60 @@ function setupCounter(total) {
         format: total.toLocaleString(),
         stop: total.toLocaleString()
     });
-
-
 }
+
+function gallonsPerStationSeries(data) {
+    var years = {};
+    var yearsList = [];
+    // TODO: Change 2016 to current year to something more dynamic.
+    for (var year = 1985; year < 2016; year++) {
+        years[year] = 0;
+        yearsList.push(year);
+    }
+
+    var stations = {
+        crcw: {
+            name: 'Downtown',
+            years: years
+        },
+        obrien: {
+            name: 'Calumette',
+            years: years
+        },
+        wilmette: {
+            name: 'Wilmette',
+            years: years
+        }
+    };
+
+    for (var i = 0; i < data.length; i++) {
+        var event = data[i];
+
+        stations['crcw']['years'][parseInt(event.year)] += parseFloat(event['crcw']);
+        stations['obrien']['years'][parseInt(event.year)] += parseFloat(event['obrien']);
+        stations['wilmette']['years'][parseInt(event.year)] += parseFloat(event['wilmette']);
+    }
+    
+    retVal = [];
+    $.each(stations, function (index, station) {
+        series = {name: station.name, data: []};
+        $.each(station.years, function (index, year) {
+            series['data'].push(year);
+        });
+        retVal.push(series);
+    });
+    return {series: retVal.reverse(), years: yearsList};
+}
+
+/*
+    series: [{
+        name: 'John',
+        data: [5, 3, 4, 7, 2]
+    }, {
+        name: 'Jane',
+        data: [2, 2, 3, 2, 1]
+    }, {
+        name: 'Joe',
+        data: [3, 4, 4, 2, 5]
+    }]
+*/
